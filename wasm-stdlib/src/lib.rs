a26953fd049559ee1ff4839ec6ec07ff54f06132
NewGyu <NewGyu@kinoboku.net>
2022-04-28 13:59:54 +0000

    ADD: conditional compile hints

--- wasm-stdlib/src/lib.rs
+++ wasm-stdlib/src/lib.rs
@@ -1,7 +1,8 @@
+#[cfg(all(target_arch = "wasm32", not(target_os = "wasi")))]
 use wasm_bindgen::prelude::*;
 
-#[wasm_bindgen]
-pub fn do_something(arg:u32) -> u32{
+#[cfg_attr(all(target_arch = "wasm32", not(target_os = "wasi")), wasm_bindgen)]
+pub fn do_something(arg: u32) -> u32 {
     if let 0 = arg {
         println!("arg zero is given!!");
         100 + arg
