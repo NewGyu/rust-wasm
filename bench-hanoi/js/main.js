5673cdefceee306fdaeb71dbf7ea8b811fb34653
NewGyu <NewGyu@kinoboku.net>
2022-05-07 01:08:25 +0000

    apply formatter

--- bench-hanoi/js/main.js
+++ bench-hanoi/js/main.js
@@ -4,7 +4,7 @@ import { HanoiSolver } from "./hanoi.js";
 import { solve_hanoi } from "./lib.js"
 
 if (process.argv.length < 3)
-    throw new Error("arg number of disks is required.");
+  throw new Error("arg number of disks is required.");
 
 const debug = process.argv.length > 3;
 const n = parseInt(process.argv[2]);
--- bench-hanoi/testsite/index.js
+++ bench-hanoi/testsite/index.js
@@ -3,14 +3,14 @@ import { solve_hanoi as js_ver } from "hanoi-js";
 
 
 window["hanoi"] = {
-    "solve_with_wasm": (num_of_disks, debug) => {
-        console.time("wasm_ver");
-        wasm_ver(num_of_disks, debug);
-        console.timeEnd("wasm_ver");
-    },
-    "solve_with_js": (num_of_disks, debug) => {
-        console.time("js_ver");
-        js_ver(num_of_disks, debug);
-        console.timeEnd("js_ver");
-    }
+  "solve_with_wasm": (num_of_disks, debug) => {
+    console.time("wasm_ver");
+    wasm_ver(num_of_disks, debug);
+    console.timeEnd("wasm_ver");
+  },
+  "solve_with_js": (num_of_disks, debug) => {
+    console.time("js_ver");
+    js_ver(num_of_disks, debug);
+    console.timeEnd("js_ver");
+  }
 }
